db5cfd9bbc27a758c124e77fa35fa3db60ee6c60
shahryar <panjwanishahryar@yahoo.com>
2020-03-13 10:09:02 +0800

    Update collector.php
    updated

--- bit210/collector.php
+++ bit210/collector.php
@@ -6,7 +6,7 @@ session_start();
 ?>
 
     <head>
-        <meta charset="utf-8">
+        <meta c123harset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="Pooria Atarzadeh">
