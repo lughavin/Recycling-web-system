926279209accd19367d77dafa1156ad4cc7751f0
lughavin <ghambi.lughano@gmail.com>
2020-03-13 10:10:14 +0800

    Update collector.php
    error was header fixed

--- bit210/collector.php
+++ bit210/collector.php
@@ -6,7 +6,7 @@ session_start();
 ?>
 
     <head>
-        <meta c123harset="utf-8">
+        <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="Pooria Atarzadeh">
